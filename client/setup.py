9862e79241cc7398d6af035dc6dc25e2df085bb5
Jaime Filson <wick2o@gmail.com>
2012-11-26 13:40:25 -0500

    Adding Windows Client

---
+++ client/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import py2exe
+import os
+import sys
+
+def main():
+	setup(console = ['client.py'],
+		options = {'py2exe': {'bundle_files' : 1, 'includes' : ['poster'], 'dll_excludes': ['w9xpopen.exe'],}},
+		zipfile = None,
+		optimize = 2)
+		
+def cleanup():
+	if os.name != 'posix':
+		os.system("rmdir /S /Q build")
+		os.system("move /y dist\client.exe .")
+		os.system("rmdir /S /Q dist")
+	else:
+		os.system("rm -rf build")
+
+	
+if __name__ == "__main__":
+	main()
+	cleanup()
