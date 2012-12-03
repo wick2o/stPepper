f8f8db49b98b740f59b5a7841c2ace2ce6d8b104
Jaime Filson <wick2o@gmail.com>
2012-12-02 22:36:36 -0500

    attempt at server.exe for windows

---
+++ server/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import py2exe
+import os
+import sys
+
+def main():
+	setup(console = [{'script': 'server.py', 'icon_resources' : [(1, 'yellowsub.ico')]}],
+		options = {'py2exe': {'bundle_files' : 1, 'includes' : ['bottle'], 'dll_excludes': ['w9xpopen.exe'],}},
+		zipfile = None,
+		optimize = 2,
+		icon_resources = [(1, 'yellowsub.ico')],
+		)
+		
+def cleanup():
+	if os.name != 'posix':
+		os.system("rmdir /S /Q build")
+		os.system("move /y dist\server.exe .")
+		os.system("rmdir /S /Q dist")
+	else:
+		os.system("rm -rf build")
+
+	
+if __name__ == "__main__":
+	main()
+	cleanup()
